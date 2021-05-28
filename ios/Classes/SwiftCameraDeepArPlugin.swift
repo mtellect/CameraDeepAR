8161137ffd2dd1aed59c149e933fadbc5c58d282
Alex Z <a.zonta@simwave.ca>
2021-05-28 14:13:03 -0400

    Double

--- ios/Classes/SwiftCameraDeepArPlugin.swift
+++ ios/Classes/SwiftCameraDeepArPlugin.swift
@@ -253,7 +253,6 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
                         if let component = (dict["component"] as? String){
                             if let parameter = (dict["parameter"] as? String){
                                 if let floatValue = (dict["floatValue"] as? Double){
-                                    floatValue = (Float) floatValue;
                                    [self.arView changeParameter:@changeParameter component:@component parameter:@parameter floatValue:@floatValue]
                                 }
                             }
