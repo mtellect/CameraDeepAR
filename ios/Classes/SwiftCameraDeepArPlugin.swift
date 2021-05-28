5e6a4b10545a851c37ffec0b2cc03b97b9e6d015
Alex Z <a.zonta@simwave.ca>
2021-05-28 15:42:27 -0400

    bl

--- ios/Classes/SwiftCameraDeepArPlugin.swift
+++ ios/Classes/SwiftCameraDeepArPlugin.swift
@@ -253,16 +253,11 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
                         if let component = (dict["component"] as? String){
                             if let parameter = (dict["parameter"] as? String){
                                 if let floatValue = (dict["floatValue"] as? Double){
-                                    NSLog("deepAR Change Param");
-                                   self.deepAR.changeParameter(changeParameter,component:component,parameter:parameter,floatValue: CGFloat(floatValue));
+                                   self.arView.changeParameter(changeParameter,component:component,parameter:parameter,floatValue: Float(floatValue));
                                 }
-                                NSLog("Param");
                             }
-                            NSLog("component");
                         }
-                        NSLog("changeParam");
                     }
-                    NSLog("Arguments");
                 }
                 result("Param Changed")
             }
