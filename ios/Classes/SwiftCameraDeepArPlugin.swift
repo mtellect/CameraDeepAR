829054a2d3c67ac5059d4b38b0d5f24d3cd75aae
Alex Z <a.zonta@simwave.ca>
2021-07-26 14:48:44 -0400

    first ios test

--- ios/Classes/SwiftCameraDeepArPlugin.swift
+++ ios/Classes/SwiftCameraDeepArPlugin.swift
@@ -295,8 +295,9 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
                         //let view = self.arView.inputView;
                         //view?.backgroundColor = UIColor.red;
                         let flashView = UIView(frame: self.arView.frame)
+                        let rImage = resizeImage(image: image!, targetSize: CGSize(width: 200.0, height: 200.0));
                         flashView.alpha = 100
-                        flashView.backgroundColor = UIColor(patternImage: image!)
+                        flashView.backgroundColor = UIColor(patternImage: rImage)
                         self.arView.insertSubview(flashView, aboveSubview: self.arView)
                        
     //                                    self.deepAR.changeParameter(changeParameter,component:component,parameter:parameter,image: image);
@@ -317,6 +318,32 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
             // Fallback on earlier versions
         }
     }
+
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+    let size = image.size
+    
+    let widthRatio  = targetSize.width  / size.width
+    let heightRatio = targetSize.height / size.height
+    
+    // Figure out what our orientation is, and use that to form the rectangle
+    var newSize: CGSize
+    if(widthRatio > heightRatio) {
+        newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+    } else {
+        newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+    }
+    
+    // This is the rect that we've calculated out and this is what is actually used below
+    let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+    
+    // Actually do the resizing to the rect using the ImageContext stuff
+    UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+    image.draw(in: rect)
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    return newImage!
+}
     
     
 //    @objc
