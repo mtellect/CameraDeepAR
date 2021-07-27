53c4a6c974043f4afb726e1639dd7667a0ccf278
Alex Z <a.zonta@simwave.ca>
2021-07-27 08:02:41 -0400

    Working on IOS

--- ios/Classes/SwiftCameraDeepArPlugin.swift
+++ ios/Classes/SwiftCameraDeepArPlugin.swift
@@ -298,7 +298,9 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
                         //let flashView = UIView(frame: self.arView.frame)
                         let uImage = UIImageView(image: image);
                         
-                            uImage.contentMode = .scaleToFill
+                            uImage.contentMode = .scaleAspectFill
+                            uImage.tintColor = .none
+                            uImage.tintAdjustmentMode = .automatic
                        
                             uImage.frame = self.frame
                         NSLog("\(self.frame.size.width) \(self.frame.size.height)")
