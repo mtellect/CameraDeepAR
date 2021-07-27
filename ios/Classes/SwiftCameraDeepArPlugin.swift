c0e1c03cbd3002678445ca6a4b98eddb532262a7
Alex Z <a.zonta@simwave.ca>
2021-07-27 07:47:20 -0400

    Working on IOS

--- ios/Classes/SwiftCameraDeepArPlugin.swift
+++ ios/Classes/SwiftCameraDeepArPlugin.swift
@@ -298,7 +298,7 @@ public class DeepArCameraView : NSObject,FlutterPlatformView,DeepARDelegate{
                         //let flashView = UIView(frame: self.arView.frame)
                         let uImage = UIImageView(image: image);
                         
-                            uImage.contentMode = .scaleAspectFit
+                            uImage.contentMode = .center
                        
                             uImage.frame = self.frame
                         NSLog("\(self.frame.size.width) \(self.frame.size.height)")
